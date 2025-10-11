f582fbe486dfbfaf33decaaebb7c7442957092a5
DragonCloudBurst <katieaspivey@gmail.com>
2025-10-11 12:35:09 -0400

    update .gitignore and ui.cs

--- hacknc25/layout/ui.cs
+++ hacknc25/layout/ui.cs
@@ -2,28 +2,57 @@ using Spectre.Console;
 
 public class userInterface
 {
-    public Layout layout = new Layout("Root")
-    .SplitColumns(
+
+    public userInterface()
+    {
+        Layout layout = new Layout("Root")
+        .SplitColumns(
         new Layout("Left"),
         new Layout("Right")
             .SplitRows(
                 new Layout("Top"),
                 new Layout("Bottom")));
-    
-    public void Write()
-    {
-        // Update the left column
-        layout["Left"].Update(
-            new Panel(
+
+        layout["Left"].Ratio(3);
+        layout["Right"].Ratio(1);
+        layout["Top"].Ratio(1);
+        layout["Bottom"].Ratio(2);
+
+        var gamePanel = new Panel(
                 Align.Center(
                     // add player and level generation logic here
                     new Markup("placeholder"),
-                    VerticalAlignment.Middle))
-                .Expand());
+                    VerticalAlignment.Middle));
+            
+        var panelStats = new Panel(
+                Align.Center(
+                    // add in the actual variables here once we have implemented them!
+                    // i would like to use the bar widget eventually somehow if possible.
+                    new Markup("HP [   ]\r\n" +
+                    "ATK [   ]\r\n" +
+                    "DEF [   ]\r\n"),
 
-        
+                    VerticalAlignment.Middle));
 
-        // Render the layout
+        var panelFlavorText = new Panel(
+                Align.Center(
+                    new Markup(""),
+                    VerticalAlignment.Middle));
+
+        gamePanel.Border = BoxBorder.Ascii;
+        panelStats.Border = BoxBorder.Ascii;
+        panelFlavorText.Border = BoxBorder.Ascii;
+
+        gamePanel.Padding = new Padding(5, 5, 5, 5);
+        panelStats.Padding = new Padding(2, 2, 2, 2);
+        panelFlavorText.Padding = new Padding(1, 1, 1, 1);
+
+
+        layout["Left"].Update(gamePanel);
+        layout["Top"].Update(panelStats);
+        layout["Bottom"].Update(panelFlavorText);
+
         AnsiConsole.Write(layout);
     }
+
 }
