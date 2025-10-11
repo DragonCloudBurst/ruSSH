ddac19c81706a485f4b9b490479520e189852eab
krbreyn <krbreyn@gmail.com>
2025-10-11 11:55:15 -0400

    @ symbol moving on screen

--- hacknc25/Program.cs
+++ hacknc25/Program.cs
@@ -1,2 +1,94 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Spectre.Console;
+
+var game = new Game();
+game.Run();
+
+public class Player {
+	public int X {get; set;}
+	public int Y {get; set;}
+
+	public Player(int x, int y) {
+		X = x;
+		Y = y;
+	}
+
+	public void Move(int dx, int dy) {
+		X += dx;
+		Y += dy;
+	}
+}
+
+public class Game {
+	public Player player;
+	
+	public int WindowWidth {get;}
+	public int WindowHeight {get;}
+
+	private bool running = true;
+
+	public Game() {
+		WindowHeight = Console.WindowHeight;
+		WindowWidth = Console.WindowWidth;
+		player = new Player((WindowWidth / 2), (WindowHeight / 2));
+	}
+
+	public void Run() {
+		Console.CursorVisible = false;
+		AnsiConsole.Clear();
+
+		AnsiConsole.AlternateScreen(() => {
+			while (running) {
+				Render();
+				var key = Console.ReadKey(intercept: true);
+				Update(key);
+			}
+		});
+
+		// clean up
+		Console.CursorVisible = true;
+		AnsiConsole.Clear();
+		AnsiConsole.MarkupLine("[green]Thanks for playing![/]");
+	}
+
+	public void Render() {
+		AnsiConsole.Clear();
+
+		var output = new System.Text.StringBuilder();
+		
+		for (int y = 0; y < WindowHeight; y++) {
+			for (int x = 0; x < WindowWidth; x++) {
+				if (player.X == x && player.Y == y) {
+					output.Append("@");
+				} else {
+					output.Append(" ");
+				}
+			}
+			output.AppendLine();
+		}
+
+		AnsiConsole.Write(output.ToString());
+	}
+
+	public void Update(ConsoleKeyInfo key) {
+		if (key.Key == ConsoleKey.Escape) {
+			running = false;
+			return;
+		}
+
+		var (dx, dy) = key.Key switch {
+			ConsoleKey.W or ConsoleKey.UpArrow => (0, -1),
+            ConsoleKey.S or ConsoleKey.DownArrow => (0, 1),
+            ConsoleKey.A or ConsoleKey.LeftArrow => (-1, 0),
+            ConsoleKey.D or ConsoleKey.RightArrow => (1, 0),
+            _ => (0, 0)
+		};
+
+		var px = player.X;
+		var py = player.Y;
+
+		if ((px + dx >= 0 && px + dx < WindowWidth)
+			&& (py + dy >= 0 && py + dy < WindowHeight)) {
+			player.Move(dx, dy);
+		}
+	}
+}
--- hacknc25/obj/Debug/net9.0/hacknc25.AssemblyInfo.cs
+++ hacknc25/obj/Debug/net9.0/hacknc25.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hacknc25")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf93330d9214b067d78c99f2700a34155d70d047")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+784b108c01ba6bf9b7690e8deb46c395031c0cb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("hacknc25")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hacknc25")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
