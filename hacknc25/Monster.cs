91b7e875112146164c84cc133d77c2ed8d9a041f
DragonCloudBurst <katieaspivey@gmail.com>
2025-10-12 07:41:04 -0400

    add 3 new monsters

--- hacknc25/Monster.cs
+++ hacknc25/Monster.cs
@@ -5,9 +5,31 @@ public static class MonsterFactory {
 		return bat;
 	}
 
-	public static Actor NewGoblin(int x, int y) {
+	public static Actor NewGoblin(int x, int y)
+	{
 		var goblin = new Actor(x, y, "Goblin", 'g', 12, 4);
 		goblin.AddAI(new DumbAI(goblin));
 		return goblin;
+	}
+
+	public static Actor NewHarpy(int x, int y)
+	{
+		var harpy = new Actor(x, y, "Harpy", 'h', 15, 3);
+		harpy.AddAI(new WanderingAI(harpy));
+		return harpy;
+	}
+
+	public static Actor NewGargoyle(int x, int y)
+	{
+		var gargoyle = new Actor(x, y, "Gargoyle", 'r', 20, 2);
+		gargoyle.AddAI(new DumbAI(gargoyle));
+		return gargoyle;
+	}
+	
+	public static Actor NewDemon(int x, int y)
+	{
+		var demon = new Actor(x, y, "Demon", 'd', 30, 6);
+		demon.AddAI(new DumbAI(demon));
+		return demon;
 	}
 }
